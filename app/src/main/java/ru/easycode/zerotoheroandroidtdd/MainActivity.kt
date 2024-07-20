e8b3a4333730c8d9ca5642a23dbf3897bfdaaed2
Valery <i@vvaganov.ru>
2024-07-20 16:51:10 +0300

    Task/007 freezesTextView

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,13 +1,24 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.changeButton)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+
+        button.setOnClickListener{
+            textView.text = "I am an Android Developer!"
+        }
+
 
     }
+
 }
