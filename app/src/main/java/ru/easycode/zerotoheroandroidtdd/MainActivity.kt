03082e37b68dfa3f35f7973cb145bd05c31c6a4f
Valery <i@vvaganov.ru>
2024-07-20 14:35:42 +0300

    Task/006  the replacement of text by clicking on the button has been implemented

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var textView = findViewById<TextView>(R.id.titleTextView)
+        var button = findViewById<Button>(R.id.changeButton)
+
+        button.setOnClickListener {
+            textView.text = "I am an Android Developer!"
+        }
+
+
     }
 }
